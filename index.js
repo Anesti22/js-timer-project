be93164ca5bb0d2acc50bd1af98d25d7151aff69
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2020-04-18 14:51:56 +0200

    Fixing the start/pause bug

--- index.js
+++ index.js
@@ -6,56 +6,23 @@ const circle = document.querySelector('circle');
 const perimeter = circle.getAttribute('r') * 2 * Math.PI;
 circle.setAttribute('stroke-dasharray', perimeter);
 
-let duration = 0;
-const timer = new Timer(durationInput, startButton, pauseButton, {
+let duration = durationInput.value;
 
-    // callback methods
-    onStart(totalDuration) {
-        duration = totalDuration;
-    },
-    onTick(timeRemaining){
-        circle.setAttribute('stroke-dashoffset',
-            perimeter * timeRemaining / duration - perimeter
-        );
-    },
-    onComplete(){
-        console.log('Timer is completed');
-    }
+durationInput.addEventListener("change", () => {
+  duration = durationInput.value
 });
 
+let running = false;
 
-// #1
-// The value of 'this' inside of an arrow function
-// const colors = {
-//     printColor() {
-//         console.log(this);
-
-//         const printThis = () => {
-//             console.log(this);
-//         }
-
-//         printThis();
-//     }
-// }
-
-// colors.printColor();
-
-
-
-// #2
-// call bind apply
-// const printThis = function () {
-//     console.log(this)
-// }
-
-// printThis.call({color: 'red'});
-// printThis.apply({color: 'red'});
-
-// #3
-// const colors =  {
-//     printColor() {
-//         console.log(this)
-//     }
-// }
-
-// colors.printColor();
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      'stroke-dashoffset',
+      (perimeter * timeRemaining) / duration - perimeter
+    );
+    console.log((perimeter * timeRemaining) / duration - perimeter);
+  },
+  onComplete() {
+    console.log('Timer is completed');
+  }
+});
